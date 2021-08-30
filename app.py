5c3406a872299aa79bb8dc85aae283cd83056f5d
PhilippeRambaud <phiram2306@gmail.com>
2021-08-31 00:50:26 +0200

    Update __init__ and app.py to run webapp

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
+
