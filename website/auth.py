ea1338567fb01eabd25b3bbea5203dcc42e5905e
PhilippeRambaud <phiram2306@gmail.com>
2021-08-31 02:11:03 +0200

    Flashed messages for sign up page (errors and success)

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,22 @@ def login():
 def logout():
     return "<p>Log out</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('Firstname must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match .', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 6 characters.', category='error')
+        else:
+            flash('Account created.', category='success')
     return render_template("sign_up.html")
