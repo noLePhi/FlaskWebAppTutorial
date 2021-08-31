c1db945bc558b2ccb586d15a041ce0d4b3dcc2fa
PhilippeRambaud <phiram2306@gmail.com>
2021-08-31 11:39:34 +0200

    Turn the website into a blog with creation of post, likes, deleting, etc.

--- website/__init__.py
+++ website/__init__.py
@@ -6,24 +6,25 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'my_secret_key'
+    app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User, Note
+    from .models import User, Post, Comment, Like
 
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -32,7 +33,8 @@ def create_app():
 
     return app
 
+
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists("website/" + DB_NAME):
         db.create_all(app=app)
-        print('Created database!')
+        print("Created database!")
